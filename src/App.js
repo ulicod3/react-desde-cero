215aeb63fac463eb0b86f47b97e565c98e92951c
ulises <ulisescamps9@gmail.com>
2019-08-09 18:02:01 -0500

    Primer commit del curso React.js desde cero

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/styles.scss';
+import Curso from './Curso'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+<>
+<div className="main-banner img-container l-section" id="main-banner">
+  <div className="ed-grid lg-grid-6">
+    <div className="lg-cols-4 lg-x-2">
+      <img className="main-banner__img" alt="banner" src="https://miro.medium.com/max/3840/1*vXjx4Y7EgQBwALkaeZJ0Cg.jpeg"/>
+      <div className="main-banner__data s-center">
+        <p className="t2 s-mb-0">Cursos de uliCode</p>
+        <p> Tu futuro te esta esperando</p>
+        <a href="https://ed.team/" className="button">Empieza ya</a>
+      </div>
     </div>
-  );
-}
+  </div>
+</div>
 
+<div className="ed-grid m-grid-3">
+<Curso/>
+<Curso/>
+<Curso/>
+<Curso/>
+</div>
+</>
+)
 export default App;
