5014aad3665f9f3dece5553b32bd0bae4b15e285
ulises <ulisescamps9@gmail.com>
2019-08-09 22:35:01 -0500

    Recorriendo arrays

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import React from 'react';
 import './styles/styles.scss';
 import Curso from './Curso'
 
+
+const cursos = [
+  {
+    "title": "React desde cero",
+    "image": "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
+    "price": 30,
+    "profesor": "Beto Quiroga"
+  }, 
+  {"title": "Java Bases de datos",
+  "image": "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/java-bases-de-datos.jpg?itok=2cArgO8s",
+  "price": 50,
+  "profesor": "Beto Quiroga"}, 
+  {"title": "Fundamentos de Redes",
+  "image": "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-03/Redes%20Fundamentos.png",
+  "price": 20,
+  "profesor": "Neil Ulises"},
+  {"title": "HTML desde cer",
+  "image": "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/HTML-2018.jpg?itok=Gyvm-W9t",
+  "price": 0,
+  "profesor": "Alvaro Felipe"}
+]
 const App = () => (
 <>
 <div className="main-banner img-container l-section" id="main-banner">
@@ -18,10 +39,9 @@ const App = () => (
 </div>
 
 <div className="ed-grid m-grid-3">
-<Curso/>
-<Curso/>
-<Curso/>
-<Curso/>
+{
+  cursos.map(c => <Curso title={c.title} image={c.image} price={c.price} profesor={c.profesor}/>)
+}
 </div>
 </>
 )
