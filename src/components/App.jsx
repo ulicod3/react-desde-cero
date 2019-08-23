875189e824efa6c40269066ab5f7d5928ea15bbb
ulises <ulisescamps9@gmail.com>
2019-08-22 23:59:07 -0500

    Eliminando incosistencias

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,23 @@ import Home from './Pages/Home'
 // eslint-disable-next-line
 import Curso from './Molecules/CourseCard'
 import Form from "./Pages/Form"
-import CourseGrid from "./Organisms/CourseGrid";
+import Courses from "./Pages/Courses"
 import Course from "./Pages/Course"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 // eslint-disable-next-line
 import History from "./Pages/History"
-import MainMenu from '../MainMenu'
+import MainMenu from './Organisms/MainMenu'
+import Users from "./Pages/Users"
 const App = () => (
 <Router>
     <MainMenu/>
     <Switch>
 <Route path="/" exact component={ Home }/>
 <Route path="/cursos/:id" component={ Course }/>
-<Route path="/cursos" exact component={ CourseGrid }/>
+<Route path="/cursos"  component={ Courses}/>
 <Route path="/historial" component={History}/>
 <Route path="/historial/:valor" component={History}/>
+<Route path="/usuarios" component={Users}/> 
 <Route path="/formulario" exact component={ () => <Form/> }/>
 <Route component={() => (
     <div className="ed-grid">
--- src/components/Organisms/CourseGrid.jsx
+++ src/components/Organisms/CourseGrid.jsx
@@ -1,49 +1,22 @@
 import React from "react"
-import CourseCard from "../Molecules/CourseCard";
+import CourseCard from "../Molecules/CourseCard"
 
-
-const courses = [
-{
-  "id": 1,
-  "title": "React desde cero",
-  "image": "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
-  "price": 30,
-  "professor": "Alexix Lozada"
-}, 
-{
- "id": 2, 
-"title": "Java Bases de datos",
-"image": "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/java-bases-de-datos.jpg?itok=2cArgO8s",
-"price": 50,
-"professor": "Beto Quiroga"}, 
-{
- "id": 3, 
-"title": "Fundamentos de Redes",
-"image": "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-03/Redes%20Fundamentos.png",
-"price": 20,
-"professor": "Neil Ulises"},
-{
- "id": 4, 
-"title": "HTML desde cero",
-"image": "https://drupal.ed.team/sites/default/files/styles/medium/public/courses/images/HTML-2018.jpg?itok=Gyvm-W9t",
-"price": 0,
-"professor": "Alvaro Felipe"}
-]
-
-
-const CourseGrid = () => (
+const CourseGrid = ({courses}) => (
     <div className="ed-grid m-grid-4">
-        { courses.map(c => (
-        <CourseCard
-         key={c.id}
-         id={c.id}
-         title={c.title}
-         image={c.image}
-         price={c.price}
-         professor={c.professor}
-        />
-        )) }
-    </div>
+        {
+       courses.length === 0 
+    ? <h1 className="t3">Cargando..</h1>
+    : courses.map(c => (
+      <CourseCard
+       key={c.id}
+       id={c.id}
+       title={c.title}
+       image={c.image}
+       price={c.price}
+       professor={c.professor}
+      />
+      )) 
+    }
+</div>
 )
-
-export default CourseGrid 
+export default CourseGrid
