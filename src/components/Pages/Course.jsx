8e1e543c8901250f16f5f95a40cbd0e5520a472c
ulises <ulisescamps9@gmail.com>
2019-08-24 00:59:03 -0500

    Componentes con multiples estados

--- src/components/Pages/Course.jsx
+++ src/components/Pages/Course.jsx
@@ -4,23 +4,36 @@ import axios from "axios";
 
 const Course = ({ match }) => {
 
-    const [state, setState] = useState({})
+    const [course, setCourse] = useState({})
+    const [comment, setComment] = useState("Sin comentarios")
 
-    useEffect(() => {
-    axios.get('https://my-json-server.typicode.com/UlisesCampos/JSON-server/cursos/4')
-    .then(response => setState({response.data))
-    })
+    useEffect(() => {   
+    axios.get(`https://my-json-server.typicode.com/UlisesCampos/JSON-server/cursos/${match.params.id}`)
+    .then(resp => setCourse(resp.data))
+   // eslint-disable-next-line 
+    }, [])
+
+    const myComment = e => {
+        setComment(e.target.value)
+    }
 
     return (
         <div className="ed-grid m-grid-3">
         {
-            state ? (
-                <>
-                    <h1 className="m-cols-3"> { state.title } </h1>
-                    <img className="m-cols-1" src={ state.image } alt={state.titulo}/>
-                    <p className="m-cols-2">Profesor: {state.profesor} </p>
-                    <button onClick={changeTitle.bind(this, "Go desde cero")}>Cambiar titulo</button>
-                </> 
+            course ? (
+                <div className="ed-grid">
+                 <div className="l-block ">
+                    <h1 className="m-cols-3"> { course.title } </h1>
+                    <img className="m-cols-1" src={ course.image } alt={course.title}/>
+                    <p className="m-cols-2">Profesor: {course.professor} </p>
+                 </div> 
+                 <div className="ed-grid">
+                    <h2>Escribe tu comentario</h2>
+                    <input type="text" placeholder="Escribe ..." onChange={myComment.bind(this)} />
+                    <p>{comment}</p>
+                 </div>  
+                   
+                </div> 
                 ) : 
                  <h1>El curso no existe</h1>        
         }
